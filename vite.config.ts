b1b64c70059ca82b128cf6a6e55ebb585d63f9f6
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2024-01-09 19:18:46 -0300

    chore: deps cleanup and change to pnpm

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig((configEnv) => {
-  const isDevelopment = configEnv.mode === "development";
   const env = loadEnv(configEnv.mode, process.cwd(), "");
 
   return {
@@ -12,23 +11,11 @@ export default defineConfig((configEnv) => {
     server: {
       port: 3000,
     },
-    test: {
-      globals: true,
-      environment: "happy-dom",
-      setupFiles: "./src/infrastructure/tests.setup.ts",
-    },
     resolve: {
       alias: {
         app: resolve(__dirname, "src", "app"),
         components: resolve(__dirname, "src", "components"),
         hooks: resolve(__dirname, "src", "hooks"),
-      },
-    },
-    css: {
-      modules: {
-        generateScopedName: isDevelopment
-          ? "[name]__[local]__[hash:base64:5]"
-          : "[hash:base64:5]",
       },
     },
   };
